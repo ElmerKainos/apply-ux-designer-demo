645e1c2a4b8b4c44ea124bf47ff5788b853a837e
ElmerKainos <123182590+ElmerKainos@users.noreply.github.com>
2024-01-22 12:10:21 +0000

    sffs

--- app/routes.js
+++ app/routes.js
@@ -16,5 +16,35 @@ res.redirect ('/new/code')
 router.post('/new/code' , (req,res) => {
     res.redirect ('/new/name')
     
+})
+router.post('/new/name' , (req,res) => {
+        res.redirect ('/new/experience')
+        
+        
+})
+        
+ router.post('/new/experience' , (req,res) => {
+
+
+
+    if(req.body.new.experience == 'None'){
+    res.redirect ('/new/not-eligible')
+
+
+     } else {
+           
+     res.redirect ('/new/expertise')
+     }     
+ })
+
+ router.post('/new/expertise' , (req,res) => {
+    res.redirect ('/new/skills')
     
-    })
+    
+})
+
+router.post('/new/skills' , (req,res) => {
+    res.redirect ('/new/check')
+    
+    
+})
